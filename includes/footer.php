f0b67361714126187637549e569a6c59a5050c5a
kreid333 <kai.j.reid@gmail.com>
2021-01-11 13:37:46 -0500

    completed index page and created footer

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer mt-5">
+    <div class="container">
+        <p id="footer-text">&copy; 2020, Johson Photo</p>
+    </div>
+</footer>
--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,38 @@
-<header>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Johson Photo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</head>
+<style>
+  .carousel-item {
+    height: 500px;
+  }
+
+  .carousel-item img {
+    height: 500px;
+    object-position: top;
+    object-fit: cover;
+  }
+
+  .footer {
+    padding: 20px;
+    background-color: #f8f9fa;
+    color: black;
+  }
+
+  #footer-text {
+    text-align: center;
+    margin-bottom: 0;
+  }
+</style>
+
+<body>
+  <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
         <a class="navbar-brand" href="#"><img src="./images/logo.png" alt="Johnson Photo logo" width="100px" height="50px" /></a>
@@ -7,9 +41,9 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
-            <a class="nav-link" aria-current="page" href="#">Home</a>
-            <a class="nav-link" href="#">Portfolio</a>
-            <a class="nav-link" href="#">Contact</a>
+            <a class="nav-link" aria-current="page" href="index.php">Home</a>
+            <a class="nav-link" href="portfolio.php">Portfolio</a>
+            <a class="nav-link" href="contact.php">Contact</a>
           </div>
         </div>
       </div>
